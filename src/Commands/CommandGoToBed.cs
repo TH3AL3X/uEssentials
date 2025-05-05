1672ffad1adc2b03a414f52643c34ed0e4e41b65
TH3AL3X <themachinehack@gmail.com>
2025-05-05 19:23:22 +0200

    Rework gotobed

--- src/Commands/CommandGoToBed.cs
+++ src/Commands/CommandGoToBed.cs
@@ -21,6 +21,7 @@
 */
 #endregion
 
+using System.Collections.Generic;
 using System.Linq;
 using Essentials.Api;
 using Essentials.Api.Command;
@@ -33,6 +34,8 @@ using Essentials.I18n;
 using Rocket.Unturned.Chat;
 using Rocket.Unturned.Player;
 using SDG.Unturned;
+using Steamworks;
+using UnityEngine;
 
 namespace Essentials.Commands
 {
@@ -43,33 +46,74 @@ namespace Essentials.Commands
         Description = "Go to the bed of a player",
         AllowedSource = AllowedSource.PLAYER,
         MinArgs = 1,
-        MaxArgs = 1
+        MaxArgs = 2
     )]
     public class CommandGoToBed : EssCommand
     {
-
-        public override CommandResult OnExecute(ICommandSource src, ICommandArgs args)
+        private List<(Vector3, byte)> GetBeds(UPlayer player)
         {
-            if (args.Length == 0)
+            var results = new List<(Vector3, byte)>();
+
+            for (byte x = 0; x < BarricadeManager.regions.GetLength(0); x++)
             {
-                return CommandResult.ShowUsage();
+                for (byte y = 0; y < BarricadeManager.regions.GetLength(1); y++)
+                {
+                    var region = BarricadeManager.regions[x, y];
+
+                    for (int i = 0; i < region.barricades.Count; i++)
+                    {
+                        var drop = region.drops[i];
+                        var barricade = region.barricades[i];
+
+                        if (drop?.interactable is InteractableBed bed && (CSteamID)barricade.owner == player.CSteamId)
+                        {
+                            results.Add((bed.transform.position, 0));
+                        }
+                    }
+                }
             }
 
-            if (!UPlayer.TryGet(args[0].ToString(), out var player))
+            return results;
+        }
+        public override CommandResult OnExecute(ICommandSource src, ICommandArgs args)
+        {
+            if (!args[0].IsValidPlayerIdentifier)
             {
                 return CommandResult.LangError("PLAYER_NOT_FOUND", args[0]);
             }
 
-            if (BarricadeManager.tryGetBed(player.CSteamId, out var Location, out byte Angle))
-            {
-                src.ToPlayer().UnturnedPlayer.teleportToLocationUnsafe(Location, Angle);
-                EssLang.Send(src, "GOTOBED");
-            }
-            else
-            {
+            var player = args[0].ToPlayer;
+
+            var beds = GetBeds(player);
+
+            if (beds.Count == 0)
                 return CommandResult.LangError("GOTOBED_NOT_FOUND");
+
+            if (beds.Count == 1)
+            {
+                src.ToPlayer().UnturnedPlayer.teleportToLocationUnsafe(beds[0].Item1, 0);
+                EssLang.Send(src, "GOTOBED");
+                return CommandResult.Success();
             }
 
+            if (args.Length == 2 && int.TryParse(args[1].ToString(), out int index))
+            {
+                index -= 1;
+                if (index >= 0 && index < beds.Count)
+                {
+                    var (loc, angle) = beds[index];
+                    src.ToPlayer().UnturnedPlayer.teleportToLocationUnsafe(loc, 0);
+                    EssLang.Send(src, "GOTOBED");
+                    return CommandResult.Success();
+                }
+                else
+                {
+                    return CommandResult.LangError("GOTOBED_INVALID_INDEX");
+                }
+            }
+
+            var bedList = string.Join(", ", beds.Select((b, i) => $"{i + 1}"));
+            EssLang.Send(src, "GOTOBED_MULTIPLE", bedList, player.CSteamId);
             return CommandResult.Success();
         }
     }
